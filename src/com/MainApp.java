38e5602c9e490a2b24714c5a243bd2acd17c9a03
kevinari26 <kevin_arisaputra@yahoo.com>
2022-09-30 19:39:15 +0700

    .

--- src/com/MainApp.java
+++ src/com/MainApp.java
@@ -5,12 +5,20 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
  
-@Path("/test")
+
+@Path("/root")
 public class MainApp {
  
     @GET
+    @Path("/test")
     @Produces(MediaType.TEXT_PLAIN)
     public String direBonjour() {
         return "Bonjour, tout le monde!";
     }
 }
+
+//@GET
+//@Produces(MediaType.APPLICATION_JSON)
+//public String sayJsonHello() {
+//    return "{\"name\":\"greeting\", \"message\":\"Bonjour tout le monde!\"}";
+//}
